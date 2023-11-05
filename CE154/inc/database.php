84a4de3ca12c68dcd3bad1d2eabd322a75b7a453
Ollie <44728079+OliMations@users.noreply.github.com>
2023-11-05 18:39:09 +0000

    Removing CE154

--- CE154/inc/database.php
+++
@@ -1,7 +0,0 @@
-<!-- 2205667 -->
-<?php
-    $conn = new mysqli("localhost", "os22495", "oqb49c3LFjIvN", "ce154_os22495");
-    if ($conn->connect_error) {
-        die("Connection Refused: " . $conn->connect_error);
-    }
-?>
--- CE154/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- 2205667 -->
-<footer>
-        <div class="container row">
-            <p class="copyright">© Copyright 2023 Glass Castle</p>
-        </div>
-    </footer>
-</body>
-</html>
